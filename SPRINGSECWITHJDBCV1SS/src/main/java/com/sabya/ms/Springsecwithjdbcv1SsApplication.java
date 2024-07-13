6b0868b1e4e1eb20e14738cd8b98c2d3c588a5ac
Sabya <sabya.workspace@gmail.com>
2024-07-13 11:23:21 +0530

    Initial commit
    Initial commit

---
+++ SPRINGSECWITHJDBCV1SS/src/main/java/com/sabya/ms/Springsecwithjdbcv1SsApplication.java
@@ -0,0 +1,13 @@
+package com.sabya.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecwithjdbcv1SsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecwithjdbcv1SsApplication.class, args);
+	}
+
+}
